71dd86589802a9c16b4a4865a1b46870a3e0da96
Matias Ventura <matias.rojas@grupodamez.com.ar>
2019-11-15 15:30:03 -0300

    Add project files.

---
+++ SAL_Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAL_Core
+{
+    public class Class1
+    {
+    }
+}
